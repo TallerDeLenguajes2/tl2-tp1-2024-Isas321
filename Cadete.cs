1e18c61d30434c1ee38ad453524908b1a3ce1c72
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2024-08-23 11:57:25 -0300

    funcion para asignar pedidos y cambio de nombres

--- Cadete.cs
+++ Cadete.cs
@@ -1,32 +1,33 @@
 namespace EspacioCadeteria
 {
+
     public class Cadete
   {
     private string Id;
     private string Nombre;
     private string Direccion;
     private string Telefono;
-    private List<Pedidos> ListadoPedidos;
+    private List<Pedidos> Pedidos;
 
-    public Cadete(string id, string nombre, string direccion, string telefono, List<Pedidos> listadoPedidos)
+    public Cadete(string id, string nombre, string direccion, string telefono, List<Pedidos> pedidos)
     {
         Id1 = id;
         Nombre1 = nombre;
         Direccion1 = direccion;
         Telefono1 = telefono;
-        ListadoPedidos1 = listadoPedidos;
+        Pedidos1 = pedidos;
     }
 
     public string Id1 { get => Id; set => Id = value; }
     public string Nombre1 { get => Nombre; set => Nombre = value; }
     public string Direccion1 { get => Direccion; set => Direccion = value; }
     public string Telefono1 { get => Telefono; set => Telefono = value; }
-    public List<Pedidos> ListadoPedidos1 { get => ListadoPedidos; set => ListadoPedidos = value; }
+    public List<Pedidos> Pedidos1 { get => Pedidos; set => Pedidos = value; }
   
     public double JornalACobrar(){
       int contador=0;
       double pagoPorPedido = 500;
-      foreach (var pedido in ListadoPedidos1)
+      foreach (var pedido in Pedidos1)
       {
         contador++;
       }
--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,21 +1,50 @@
+using System.Linq;
 namespace EspacioCadeteria
 {
 
-    public class Cadeteria
+  public class Cadeteria
   {
     private string Nombre;
     private string Telefono;
-    private List<Cadete> ListadoCadetes;
+    private List<Cadete> Cadetes; //Por convencion se llama solo cadetes en plural, no poner ListadoCadetes
 
-    public Cadeteria(string nombre, string telefono, List<Cadete> listadoCadetes)
+    // public Cadete cadete1 {get; set}
+    public Cadeteria(string nombre, string telefono, List<Cadete> Cadetes)
     {
         Nombre1 = nombre;
         Telefono1 = telefono;
-        ListadoCadetes1 = listadoCadetes;
+        Cadetes1 = Cadetes;
     }
 
     public string Nombre1 { get => Nombre; set => Nombre = value; }
     public string Telefono1 { get => Telefono; set => Telefono = value; }
-    public List<Cadete> ListadoCadetes1 { get => ListadoCadetes; set => ListadoCadetes = value; }
+    public List<Cadete> Cadetes1 { get => Cadetes; set => Cadetes = value; }
+
+    public Pedidos DarDeAltaPedido(string nro, string obs, string nombre_Cli, string direccion_Cli, string telefono_Cli, string datosRefDireccion_Cli, string estado){
+      Pedidos Pedido = new Pedidos(nro, obs, nombre_Cli, direccion_Cli, telefono_Cli, datosRefDireccion_Cli, estado);
+      return Pedido;
     }
+
+    public Cadete CadeteConMenosPedidos(List<Cadete> Cadetes1)
+    {
+      Cadete cadeteConMenosPedidos = Cadetes1.MinBy(cadete => cadete.Pedidos1.Count);
+
+      if (cadeteConMenosPedidos != null)
+      {
+          Console.WriteLine($"El cadete con menos pedidos es {cadeteConMenosPedidos.Nombre1} con {cadeteConMenosPedidos.ListadoPedidos1.Count} pedidos.");
+      }
+      else
+      {
+          Console.WriteLine("No hay cadetes disponibles.");
+      }
+
+      return cadeteConMenosPedidos;
+    }
+
+    public Cadete AsignarPedidoCadete(Pedidos pedido){
+      Cadete cadete = CadeteConMenosPedidos(Cadetes1);
+      cadete.Pedidos1.Add(pedido);
+      return cadete;
+    }
+  }
 }
