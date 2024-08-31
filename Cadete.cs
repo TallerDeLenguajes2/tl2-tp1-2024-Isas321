2e2cae53d6a34d0f3c9fba21a9f20d7b2a2c3acf
Isas321 <javier.isas321@gmail.com>
2024-08-30 23:45:15 -0300

    buenas practicas

--- Cadete.cs
+++ Cadete.cs
@@ -3,31 +3,31 @@ namespace EspacioCadeteria
 
     public class Cadete
   {
-    private string Id;
-    private string Nombre;
-    private string Direccion;
-    private string Telefono;
-    private List<Pedido> Pedidos;
+    private string id;
+    private string nombre;
+    private string direccion;
+    private string telefono;
+    private List<Pedido> pedidos;
 
     public Cadete(string id, string nombre, string direccion, string telefono, List<Pedido> pedidos)
     {
-        Id1 = id;
-        Nombre1 = nombre;
-        Direccion1 = direccion;
-        Telefono1 = telefono;
-        Pedidos1 = pedidos;
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.pedidos = pedidos;
     }
 
-    public string Id1 { get => Id; set => Id = value; }
-    public string Nombre1 { get => Nombre; set => Nombre = value; }
-    public string Direccion1 { get => Direccion; set => Direccion = value; }
-    public string Telefono1 { get => Telefono; set => Telefono = value; }
-    public List<Pedido> Pedidos1 { get => Pedidos; set => Pedidos = value; }
+    public string Id { get => id; set => id = value; }
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Direccion { get => direccion; set => direccion = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+    public List<Pedido> Pedidos { get => pedidos; set => pedidos = value; }
   
     public double JornalACobrar(){
       int contador=0;
       double pagoPorPedido = 500;
-      foreach (var pedido in Pedidos1)
+      foreach (var pedido in Pedidos)
       {
         contador++;
       }
