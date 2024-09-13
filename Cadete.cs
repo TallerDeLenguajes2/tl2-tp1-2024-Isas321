cb301490b02cfd2ba9583fb4175347ffa465f07c
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2024-09-13 11:33:21 -0300

    se quita la lista de pedidos de cadetes con sus metodos

--- Cadete.cs
+++ Cadete.cs
@@ -1,58 +1,30 @@
 namespace EspacioCadeteria
 {
-
-    public class Cadete
+  public class Cadete
   {
     private string id;
     private string nombre;
     private string direccion;
     private string telefono;
-    private List<Pedido> pedidos;
 
-    public Cadete(string id, string nombre, string direccion, string telefono, List<Pedido> pedidos)
+    public Cadete(string id, string nombre, string direccion, string telefono)
     {
         this.id = id;
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
-        this.pedidos = pedidos;
     }
 
     public string Id { get => id; }
     public string Nombre { get => nombre; }
     public string Direccion { get => direccion; }
     public string Telefono { get => telefono; }
-    public List<Pedido> Pedidos { get => pedidos; }
 
     public void MostrarCadete(){
       System.Console.WriteLine("\n\tId: "+Id);
       System.Console.WriteLine("\tNombre: "+Nombre);
       System.Console.WriteLine("\tDireccion: "+Direccion);
       System.Console.WriteLine("\tTelefono: "+Telefono);
-      foreach (var pedido in Pedidos)
-      {
-          pedido.MostrarPedido();
-      }
-    }
-  
-    public double JornalACobrar()
-    {
-        double pagoPorPedido = 500;
-        return ContadorPedidosEntregados() * pagoPorPedido;
-    }
-
-    public int ContadorPedidosEntregados()
-    {
-      int contador = 0;
-      foreach (var pedido in Pedidos)
-      {
-          if (pedido.EstadoPedido == Estado.Entregado)
-          {
-              contador++;
-          }
-      }
-
-      return contador;
     }
   }
 }
